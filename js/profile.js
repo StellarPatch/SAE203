29d5452f41509be54be3914bdb5a2e1f3c9032eb
StellarPatch <william.lin1708@gmail.com>
2026-05-26 01:04:27 +0200

    SAE203 V23.5

--- js/profile.js
+++ js/profile.js
@@ -10,7 +10,55 @@ const main = document.querySelector("main");
 
 btnMenu.addEventListener("click", menu);
 
+///////////////////// SCRIPT LECTEUR MUSIQUE ///////////////////
+var NomMusics = ["Ripples of Past Reverie", "Perfect Idol", "String Theory"]
+// Liste de nom de musique + auteur dans le dossier Music à modifier manuellement pour nveau
+
+let Liste_Audio = '<option value="false">None Music</option>'
+// Variable pour liste sélecteur de musique
+
+let Liste_Audio_Asset = ""
+// Variable pour ajouter physiquement sur la page HTML les audios pour les jouer (invisbile)
+
+NomMusics.forEach(function (Nom, i) {
+    Liste_Audio += '<option value="audio' + (i) + '">' + NomMusics[i] + "</option>";
+});
+document.querySelector("#Audio").innerHTML = Liste_Audio;
+// Ajout dynamique depuis NomMusics (Json pas encore) dans le selecteur de musique à jouer
+
+NomMusics.forEach(function (Nom, i) {
+    Liste_Audio_Asset += '<audio id="audio' + i + '" src="sfx/music/' + NomMusics[i] + '.mp3"></audio>'
+});
+document.querySelector("#ListeAudio").innerHTML = Liste_Audio_Asset;
+// Ajout dynamique des balises audio avec NomMusics
+
+var currentAudio = document.querySelector("#Audio").value;
+var SaveCurrent = null;
+// Variable pour acquérir musique pour audio()
+
+
 /////////////////////////////////// ZONE FONCTIONS ////////////////////////////////////////
+
+function audio() {
+    currentAudio = document.querySelector("#Audio").value;
+
+    if (SaveCurrent && SaveCurrent !== currentAudio) {
+        let prev = document.getElementById(SaveCurrent);
+        prev.pause();
+        prev.currentTime = 0;
+        prev.loop = false;
+    }
+
+    // Jouer la nouvelle musique
+    if (currentAudio !== "false") {
+        let a = document.getElementById(currentAudio);
+        a.volume = 1;
+        a.loop = true;
+        a.play();
+        SaveCurrent = currentAudio; // sauvegarder la musique actuelle
+    }
+
+}
 
 function start() {
     // fait disparaitre "l'intro"
